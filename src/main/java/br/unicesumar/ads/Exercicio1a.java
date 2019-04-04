aec7b43b03a097ee09f3c87d4d1882fd88f3b7ea
João Pedro <joao-pedro-f@hotmail.com>
2019-04-03 22:08:29 -0300

    Update Exercicio1a.java

--- src/main/java/br/unicesumar/ads/Exercicio1a.java
+++ src/main/java/br/unicesumar/ads/Exercicio1a.java
@@ -1,19 +1,42 @@
-package br.unicesumar.ads;
+﻿package br.unicesumar.ads;
 
 import java.util.Scanner;
 
 public class Exercicio1a implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
+     
+        double VeloMe;
+        double TempGasto;
+        double aut12;
+        double distancia;
+        double litUsados;
+        aut12=12;
+        
+        
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("entre com o  tempo gasto ");
+         TempGasto= teclado.nextDouble();
+        System.out.println("entre com a velocidade media ");
+        VeloMe= teclado.nextDouble();
+        
+        distancia=VeloMe*TempGasto;
+        
+        
+        litUsados=distancia/aut12;
+        System.out.println("a distancia percorrida e "+distancia);
+        System.out.println("O total de litros usados "+litUsados);
 
 
 
 
-        String resultado = "Código não implementado";
+
+String str = Double.toString(litUsados);
+String stri = Double.toString(distancia);
+
+
+
+        String resultado = "Litros usados: " + litUsados + " Distancia: " + distancia;
         return resultado;
     }
 }
