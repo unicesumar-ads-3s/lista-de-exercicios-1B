78d9628412157ab1d62749723e0d101bf3ab3fa8
tiagozamp <tzampieri7@gmail.com>
2019-04-03 22:26:29 -0300

    ex.1a e 1b

--- src/main/java/br/unicesumar/ads/Exercicio1a.java
+++ src/main/java/br/unicesumar/ads/Exercicio1a.java
@@ -3,17 +3,39 @@ package br.unicesumar.ads;
 import java.util.Scanner;
 
 public class Exercicio1a implements InterfaceExercicio {
+    Integer speed;   
+    Integer duration;
+    double distance;
+    
+    public double dist(Integer duration, Integer speed) {
+        double distancia = this.duration * this.speed;
+        return distancia;
+    }
+    
+    public String litrosUsados (double distancia) {
+        double result = distancia / 12;
+        return Double.toString(result);
+    }
+    
+    
     @Override
     public String run() {
         // Aqui vai a implementação do exercicio
         // O método deve retornar uma string
         // Caso o resultado seja um valor numérico, o valor deve
         // ser convertido em String.
+        
+        Scanner scanner = new Scanner(System.in);
+        Exercicio1a app = new Exercicio1a();
+        
+        System.out.println("Qual foi a duracao?");
+        app.duration = scanner.nextInt();
+        System.out.println("Qual foi a velocidade media?");
+        app.speed = scanner.nextInt();
+        
+        app.distance = app.dist(app.duration, app.speed);
+        
 
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+        return "Litros Usados " + app.litrosUsados(app.distance);
     }
 }
--- src/main/java/br/unicesumar/ads/Exercicio1b.java
+++ src/main/java/br/unicesumar/ads/Exercicio1b.java
@@ -3,14 +3,19 @@ package br.unicesumar.ads;
 import java.util.Scanner;
 
 public class Exercicio1b implements InterfaceExercicio {
+    Integer celsius;
+    
     @Override
     public String run() {
         // Aqui vai a implementação do exercicio
         // O método deve retornar uma string
         // Caso o resultado seja um valor numérico, o valor deve
         // ser convertido em String.
+        Scanner scanner = new Scanner(System.in);
+        Exercicio1b app = new Exercicio1b();
 
-
+        System.out.println("Qual eh a temperatura?");
+        app.celsius = scanner.nextInt();
 
 
         String resultado = "Código não implementado";
