b86121fd70dd52c9881d19494f13dfdaa55c5365
ezequiellmb <47817406+ezequiellmb@users.noreply.github.com>
2019-04-04 17:42:17 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2e.java
+++ src/main/java/br/unicesumar/ads/Exercicio2e.java
@@ -5,15 +5,43 @@ import java.util.Scanner;
 public class Exercicio2e implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
-
-
-
-
-        String resultado = "Código não implementado";
+        int n1,n2,n3,n4;
+        Scanner teclado = new Scanner (System.in);
+        System.out.println(" DIVISIVEIS POR 2 E 3 ");
+        System.out.println("Primeiro número: ");
+        n1 = teclado.nextInt();
+        System.out.println("Segundo número: ");
+        n2 = teclado.nextInt();
+        System.out.println("Terceiro número: ");
+        n3 = teclado.nextInt();
+        System.out.println("Quarto número: ");
+        n4 = teclado.nextInt();
+        String resultado;
+        if((n1%2==0)&&(n1%3==0)){
+             resultado = n1+ " Divisivel por 2 e 3";
+            System.out.println(n1+ " Divisivel por 2 e 3");
+        }
+        else if((n2%2==0)&&(n2%3==0)){
+             resultado = n2+ " Divisivel por 2 e 3";
+            System.out.println(n2+ " Divisivel por 2 e 3");
+        }
+        else if((n3%2==0)&&(n3%3==0)){
+             resultado = n3+ " Divisivel por 2 e 3";
+            System.out.println(n3+ " Divisivel por 2 e 3");
+        }
+        else if((n4%2==0)&&(n4%3==0)){
+             resultado = n4+ " Divisivel por 2 e 3";
+            System.out.println(n4+ " Divisivel por 2 e 3");
+        }
+        else {
+            resultado = "Esse número não é divisivel por 2 e 3";
+            System.out.println("Esse número não é divisivel por 2 e 3");
+        }
         return resultado;
+
+
+
+
+      
     }
 }
