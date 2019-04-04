ac217615f3814120e05d939efe85f32f6e268b6d
gabriel <grsv.romanini@gmail.com>
2019-04-03 22:36:32 -0300

    lista completa certa

--- src/main/java/br/unicesumar/ads/Exercicio1c.java
+++ src/main/java/br/unicesumar/ads/Exercicio1c.java
@@ -10,7 +10,32 @@ public class Exercicio1c implements InterfaceExercicio {
         // Caso o resultado seja um valor numérico, o valor deve
         // ser convertido em String.
 
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio3;
+import java.util.Scanner;
+/**
+ *
+ * @author UniCesumar
+ */
+public class Exercicio3 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    Scanner entrada = new Scanner(System.in);
+double Celsius, Fahrenheit;
+System.out.print("Conversor de temperatura: Graus Fahrenheit -> Graus Celsius\n\n");
+System.out.print("Digite a temperatura em Fahrenheit: ");
+Fahrenheit = entrada.nextDouble();
+Calsius = (Fahrenheit - 32 ) * 5 / 9;
+System.out.print("\n A medida convertida é " + Calsius + "ºC\n");
+}
+}
 
 
         String resultado = "Código não implementado";
