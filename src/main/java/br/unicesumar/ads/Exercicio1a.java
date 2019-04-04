d947f3b7dadaeda9f817a88669b08ccd161e197f
João Vitor de Almeida Pinto e Silva <fiirstcrw@hotmail.com>
2019-04-03 22:28:11 -0300

    Resolução Exercicio 1

--- src/main/java/br/unicesumar/ads/Exercicio1a.java
+++ src/main/java/br/unicesumar/ads/Exercicio1a.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Exercicio1a implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
+    Scanner in = new Scanner(System.in);
+        System.out.println("Digite o tempo gasto na viagem em horas: ");
+        int tempo = in.nextInt();
+        System.out.println("Digite a velocidade media durante a viagem em KM/H: ");
+        int velocidade = in.nextInt();
+
+        int distancia = tempo + velocidade;    
+
+        int litros_usados = (distancia / 12);
 
 
-
-
-        String resultado = "Código não implementado";
+        String resultado = "A velocidade media foi de " + velocidade + " a disttancia de " + distancia + " a quantidade de litros foi " + litros_usados;
         return resultado;
     }
 }
