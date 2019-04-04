6b8d2f7cc8d50237f2b8fa2717c8907fe23ed273
Edson Rodrigues <xpro23@gmail.com>
2019-04-03 22:45:33 -0300

    teste

--- src/main/java/br/unicesumar/ads/Exercicio1a.java
+++ src/main/java/br/unicesumar/ads/Exercicio1a.java
@@ -7,6 +7,8 @@ public class Exercicio1a implements InterfaceExercicio {
     public String run() {
         // Aqui vai a implementação do exercicio
         // O método deve retornar uma string
+
+        
         // Caso o resultado seja um valor numérico, o valor deve
         // ser convertido em String.
 
