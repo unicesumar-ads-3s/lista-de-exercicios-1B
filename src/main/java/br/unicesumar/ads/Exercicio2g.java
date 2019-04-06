2bc1abfe8f22ed2592799c869a04aa69f33bcf32
ezequiellmb <47817406+ezequiellmb@users.noreply.github.com>
2019-04-06 14:47:30 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2g.java
+++ src/main/java/br/unicesumar/ads/Exercicio2g.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Exercicio2g implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
-
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+        Scanner teclado = new Scanner (System.in);
+        int n;
+        
+        System.out.println("NUÚMERO ENTRE 1 E 9");
+        System.out.println("Informe um valor: ");
+        n = teclado.nextInt();
+        
+        if ((n >= 1)&&(n <= 9)){
+            System.out.println(" O valor está na faixa permitida ");
+        }
+        else {
+            System.out.println(" O valor está fora da faixa permitida ");
+        }
+        return null;
     }
 }
