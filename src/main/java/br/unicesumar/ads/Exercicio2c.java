3cb18e939b10a0a3ce02697aa051b190c07246c2
ezequiellmb <47817406+ezequiellmb@users.noreply.github.com>
2019-04-06 14:36:14 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2c.java
+++ src/main/java/br/unicesumar/ads/Exercicio2c.java
@@ -5,15 +5,28 @@ import java.util.Scanner;
 public class Exercicio2c implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
-
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+        Scanner teclado = new Scanner(System.in);
+       float nota1, nota2, nota3, nota4;
+       float media;
+        
+       System.out.println(" CALCULAR MÉDIA ");
+        System.out.println("Informe a primeira nota:");
+        nota1 = teclado.nextFloat();
+        System.out.println("Informe a primeira nota:");
+        nota2 = teclado.nextFloat();
+        System.out.println("Informe a primeira nota:");
+        nota3 = teclado.nextFloat();
+        System.out.println("Informe a primeira nota:");
+        nota4 = teclado.nextFloat();
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        String strm = String.format("%.2"+media);
+        
+        if(media >= 6){
+            System.out.println("ALUNO APROVADO \n Média: "+strm);
+        }
+        else{
+            System.out.println("ALUNO REPROVADO \n Média: "+strm);
+        }
+        return null;
     }
 }
