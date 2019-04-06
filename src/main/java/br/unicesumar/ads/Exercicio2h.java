3abaef3ee64e25b998de17538917e8b113951b72
ezequiellmb <47817406+ezequiellmb@users.noreply.github.com>
2019-04-06 15:02:12 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2h.java
+++ src/main/java/br/unicesumar/ads/Exercicio2h.java
@@ -5,15 +5,52 @@ import java.util.Scanner;
 public class Exercicio2h implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
-
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+        Scanner teclado = new Scanner (System.in);
+        int n1,n2,n3,n4,n5;
+        
+        System.out.println(" MAIOR E MENOR");
+        System.out.println("Informe um número");
+        n1 = teclado.nextInt();
+        System.out.println("Informe um número");
+        n2 = teclado.nextInt();
+        System.out.println("Informe um número");
+        n3 = teclado.nextInt();
+        System.out.println("Informe um número");
+        n4 = teclado.nextInt();
+        System.out.println("Informe um número");
+        n5 = teclado.nextInt();
+        
+        if((n1>=n2)&&(n1>=n3)&&(n1>=n4)&&(n1>=n5)){
+            System.out.println(" Maior "+n1);
+        }
+        else if((n2>=n1)&&(n2>=n3)&&(n2>=n4)&&(n2>=n5)){
+            System.out.println(" Maior "+n2);
+        }
+        else if((n3>=n1)&&(n3>=n2)&&(n3>=n4)&&(n3>=n5)){
+            System.out.println(" Maior"+n3);
+        }
+        else if((n4>=n1)&&(n4>=n2)&&(n4>=n3)&&(n4>=n5)){
+            System.out.println(" Maior "+n4);
+        }
+        else if((n5>=n1)&&(n5>=n2)&&(n5>=n3)&&(n5>=n4)){
+            System.out.println(" Maior "+n5);
+        }
+        
+        if((n1<=n2)&&(n1<=n3)&&(n1<=n4)&&(n1<=n5)){
+            System.out.println(" Menor "+n1);
+        }
+        else if((n2<=n1)&&(n2<=n3)&&(n2<=n4)&&(n2<=n5)){
+            System.out.println(" Menor "+n2);
+        }
+        else if((n3<=n1)&&(n3<=n2)&&(n3<=n4)&&(n3<=n5)){
+            System.out.println(" Menor "+n3);
+        }
+        else if((n4<=n1)&&(n4<=n2)&&(n4<=n3)&&(n4<=n5)){
+            System.out.println(" Menor "+n4);
+        }
+        else if((n5<=n1)&&(n5<=n2)&&(n5<=n3)&&(n5<=n4)){
+            System.out.println(" Menor "+n5);
+        }
+        return null;
     }
 }
