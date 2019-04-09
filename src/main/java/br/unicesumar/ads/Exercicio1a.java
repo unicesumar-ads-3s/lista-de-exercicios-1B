2a69035848540fd4e70600d3fdcec0d38a516e46
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2019-04-08 22:33:41 -0300

    first exercise complete.

--- src/main/java/br/unicesumar/ads/Exercicio1a.java
+++ src/main/java/br/unicesumar/ads/Exercicio1a.java
@@ -3,17 +3,39 @@ package br.unicesumar.ads;
 import java.util.Scanner;
 
 public class Exercicio1a implements InterfaceExercicio {
-    @Override
+    double litros_usados;
+    double distancia;
+    double tempo;
+    double velocidade;
+
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
 
 
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Coloque quanto tempo demorou: ");
+        tempo = scan.nextFloat();
+        System.out.println("Coloque a sua velocidade: ");
+        velocidade = scan.nextFloat();
+
+        getDistancia();
+        litrosUsado();
 
         String resultado = "Código não implementado";
         return resultado;
     }
+
+    public double getDistancia(){
+        distancia = tempo * velocidade;
+        System.out.println("A distancia percorrida foi: " + distancia);
+        return distancia;
+    };
+
+    public double litrosUsado(){
+        litros_usados = distancia/12;
+        System.out.println("A distancia percorrida foi: " + litros_usados);
+        return litros_usados;
+    };
+
 }
