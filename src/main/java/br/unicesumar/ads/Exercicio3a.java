e7126d63dd53c3fb6dc69b68cfc68b09aa8168f0
pesxco <49173465+pesxco@users.noreply.github.com>
2019-04-10 22:12:02 -0300

    Update Exercicio3a.java

--- src/main/java/br/unicesumar/ads/Exercicio3a.java
+++ src/main/java/br/unicesumar/ads/Exercicio3a.java
@@ -9,10 +9,31 @@ public class Exercicio3a implements InterfaceExercicio {
         // O método deve retornar uma string
         // Caso o resultado seja um valor numérico, o valor deve
         // ser convertido em String.
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+        double pi = 0.0, div = 1, erro=9999;
+        double pi_ant;
+        int i=0;
+        
+        System.out.println("Este programa calcula o pi com diferença menor que 0.000001\n");
+        
+        do{
+            pi_ant = pi;
+            if(i % 2 == 0 ){
+                pi += (4.0 / div);
+            }else{
+                pi -= 4.0 / div;
+            }
+            div = div+2;
+            i = i+1;
+            erro = abs(pi - pi_ant);
+        }while(erro > 0.00001);
+        
+        String stri = String.format("%d",i);
+        String strpi = String.format("%.6f",pi);
+        
+        System.out.println("a quantidade de interações necessarias foi: "+stri);
+        System.out.println(""+strpi);
+        
+        
+        return strpi;
     }
 }
