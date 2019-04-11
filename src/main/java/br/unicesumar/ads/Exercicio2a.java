18973f582420894a04d17b0c48027f4471052964
Kauê Schneider <kaueschneider10@gmail.com>
2019-04-11 00:06:38 -0300

    Ex java

--- src/main/java/br/unicesumar/ads/Exercicio2a.java
+++ src/main/java/br/unicesumar/ads/Exercicio2a.java
@@ -3,17 +3,38 @@ package br.unicesumar.ads;
 import java.util.Scanner;
 
 public class Exercicio2a implements InterfaceExercicio {
+    static int num1;
+    static int num2;
+    static int diferença;
+    static String resultado;
+    
+    Scanner scanf = new Scanner(System.in);
+
+    public Exercicio2a() {
+    }
+    
+    
+    
+    
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
+       
+        System.out.println("Digite um valor para o 1º numero: ");
+        num1 = scanf.nextInt();
+        System.out.println("Digite um valor para o 2º numero: ");
+        num2 = scanf.nextInt();
+        
+        if(num1 > num2){
+            diferença = num1 - num2;
+            resultado = "Numero 1º é o maior: "+num1+"   , e a diferença para o numero 2º é de: "+diferença;
+        } else if(num1 == num2){
+           resultado = "Numero 1º: " + num1 +" é igual a " + "Numero 2º: " +num2;     
+        } else{
+           diferença = num2 - num1;
+           resultado = "Numero 2º é o maior: "+num2+"    ,e a diferença para o numero 2º é de: "+diferença;
+        }
 
 
-
-
-        String resultado = "Código não implementado";
         return resultado;
     }
 }
