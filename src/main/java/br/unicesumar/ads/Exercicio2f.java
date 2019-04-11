d6a6cce417bae7e92dbd2e76e83a464c251f1228
SFerreiraLucas <47836737+SFerreiraLucas@users.noreply.github.com>
2019-04-10 22:16:12 -0300

    lista

--- src/main/java/br/unicesumar/ads/Exercicio2f.java
+++ src/main/java/br/unicesumar/ads/Exercicio2f.java
@@ -5,15 +5,35 @@ import java.util.Scanner;
 public class Exercicio2f implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
-
-
-
-
-        String resultado = "Código não implementado";
-        return resultado;
-    }
+        int num1,num2,num3,num4;
+        Scanner valor = new Scanner (System.in);
+        System.out.print("Digite 1° valor: ");
+	num1 = valor.nextInt();
+		
+	System.out.print("Digite 2° valor: ");
+	num2 = valor.nextInt();
+		
+	System.out.print("Digite 3° valor: ");
+	num3 = valor.nextInt();
+        
+        System.out.print("Digite 4° valor: ");
+	num4 = valor.nextInt();
+        
+        if((num1%2==0)||(num1%3==0)){
+            System.out.println(" Divisivel por 2 ou 3" + num1);
+        }
+        else if((num2%2==0)||(num2%3==0)){
+            System.out.println(" Divisivel por 2 ou 3" + num2);
+        }
+        else if((num3%2==0)||(num3%3==0)){
+            System.out.println(" Divisivel por 2 ou 3" + num3);
+        }
+        else if((num4%2==0)||(num4%3==0)){
+            System.out.println(" Divisivel por 2 ou 3" + num4);
+        }
+        else {
+            System.out.println("Esse número não é divisivel por 2 ou 3");
+        }
+        return null;
+     }
 }
