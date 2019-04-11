90baa63e126ee8980aa07754e0d890ca53be079f
SFerreiraLucas <47836737+SFerreiraLucas@users.noreply.github.com>
2019-04-10 22:10:05 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2d.java
+++ src/main/java/br/unicesumar/ads/Exercicio2d.java
@@ -5,10 +5,38 @@ import java.util.Scanner;
 public class Exercicio2d implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
+        Scanner valor = new Scanner (System.in);
+        int num1, num2, num3;
+
+        System.out.print("Digite 1° valor: ");
+	num1 = valor.nextDouble();
+		
+	System.out.print("Digite 2° valor: ");
+	num2 = valor.nextDouble();
+		
+	System.out.print("Digite 3° valor: ");
+	num3 = valor.nextDouble();
+
+        String strnum1=String.format("%d",num1);
+        String strnum2=String.format("%d",num2);
+        String strnum3=String.format("%d",num3);
+
+    if((num1<num2)&&(num2<num3)){
+        System.out.println("Ordem crescente é: "+strnum1+" "+strnum2+" "+strnum3);
+    }else if ((num2<num1)&&(num1<num3)){
+        System.out.println("Ordem crescente é: "+strnum2+" "+strnum1+" "+strnum3);
+    }else if ((num3<num1)&&(num1<num2)){
+        System.out.println("Ordem crescente é: "+strnum3+" "+strnum1+" "+strnum2);
+    }else if ((num3<num2)&&(num2<num1)){
+        System.out.println("Ordem crescente é: "+strnum3+" "+strnum2+" "+strnum1);
+    }else if ((num2<num3)&&(num3<num1)){
+        System.out.println("Ordem crescente é: "+strnum2+" "+strnum3+" "+strnum1);
+    }else if ((num1<num3)&&(num3<num2)){
+        System.out.println("Ordem crescente é: "+strnum1+" "+strnum3+" "+strnum2
+        );
+
+        return "";
+    }
 
 
 
