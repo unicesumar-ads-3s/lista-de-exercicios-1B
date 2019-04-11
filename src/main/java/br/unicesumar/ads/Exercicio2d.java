da4b2b462b3a2dcc2db16dffff7851255ac91a08
ezequiellmb <47817406+ezequiellmb@users.noreply.github.com>
2019-04-10 22:09:51 -0300

    Lista

--- src/main/java/br/unicesumar/ads/Exercicio2d.java
+++ src/main/java/br/unicesumar/ads/Exercicio2d.java
@@ -5,15 +5,37 @@ import java.util.Scanner;
 public class Exercicio2d implements InterfaceExercicio {
     @Override
     public String run() {
-        // Aqui vai a implementação do exercicio
-        // O método deve retornar uma string
-        // Caso o resultado seja um valor numérico, o valor deve
-        // ser convertido em String.
+        Scanner teclado = new Scanner (System.in);
+        int n1, n2, n3;
+        
+        System.out.println(" ORDEM CRESCENTE DE 3 VALORES");
+        System.out.println("Digite um numero inteiro");
+        n1 = teclado.nextInt();
+        System.out.println("Digite um numero inteiro");
+        n2 = teclado.nextInt();
+        System.out.println("Digite um numero inteiro");
+        n3 = teclado.nextInt();
 
 
-
-
-        String resultado = "Código não implementado";
-        return resultado;
+    if((n1<n2)&&(n2<n3)){
+        System.out.println("A ordem crescente é: "+n1+" "+n2+" "+n3);
+    }
+    else if ((n2<n1)&&(n1<n3)){
+        System.out.println("A ordem crescente é: "+n2+" "+n1+" "+n3);
+    }
+    else if ((n3<n1)&&(n1<n2)){
+        System.out.println("A ordem crescente é: "+n3+" "+n1+" "+n2);
+    }
+    else if ((n3<n2)&&(n2<n1)){
+        System.out.println("A ordem crescente é: "+n3+" "+n2+" "+n1);
+    }
+    else if ((n2<n3)&&(n3<n1)){
+        System.out.println("A ordem crescente é: "+n2+" "+n3+" "+n1);
+    }
+    else if ((n1<n3)&&(n3<n2)){
+        System.out.println("A ordem crescente é: "+n1+" "+n3+" "+n2);
+    }
+        return null;
     }
 }
+
